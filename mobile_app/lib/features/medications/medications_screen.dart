8b224a3b86ad0b5cfb8c8ff573b5899c64c87da7
NIHAL  T P <nihaltpnki@gmail.com>
2026-02-05 00:09:57 +0530

    refactor: clean up code formatting and improve readability in home and medications screens

--- mobile_app/lib/features/medications/medications_screen.dart
+++ mobile_app/lib/features/medications/medications_screen.dart
@@ -1,20 +1,27 @@
-import 'package:flutter/material.dart';
 import 'dart:math';
+
+import 'package:flutter/material.dart';
 
 final Random _random = Random();
 
 class MedicationsScreen extends StatefulWidget {
   const MedicationsScreen({super.key});
   static const int n = 10;
-  static List<String> medications = List.generate(n, (index) => 'Medication ${index + 1}');
-  static List<String> dosages = List.generate(n, (index) => '${(index + 1) * 10} mg');
-  static List<String> times = List.generate(n, (index) => _random.nextInt(24).toString());
-  static List<String> frequencies = List.generate(
+  static List<String> medications = List.generate(
     n,
-    (index) => List.generate(
-      7,
-      (day) => _random.nextBool() ? 'Yes' : 'No',
-    ).join(', '),
+    (index) => 'Medication ${index + 1}',
+  );
+  static List<String> dosages = List.generate(
+    n,
+    (index) => '${(index + 1) * 10} mg',
+  );
+  static List<String> times = List.generate(
+    n,
+    (index) => _random.nextInt(24).toString(),
+  );
+  static List<List<bool>> frequencies = List.generate(
+    n,
+    (index) => List.generate(7, (day) => _random.nextBool()),
   );
 
   @override
@@ -25,91 +32,94 @@ class _MedicationsScreenState extends State<MedicationsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Medications'),
-      ),
+      appBar: AppBar(title: const Text('Medications')),
       body: ListView.builder(
         itemCount: MedicationsScreen.medications.length,
         itemBuilder: (context, index) {
           return Padding(
-            padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0, bottom: 0.0),
+            padding: const EdgeInsets.only(
+              top: 8.0,
+              left: 8.0,
+              right: 8.0,
+              bottom: 0.0,
+            ),
             child: ListTile(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
               tileColor: Colors.blueAccent,
               title: Text(
                 MedicationsScreen.medications[index],
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
+                style: const TextStyle(color: Colors.white, fontSize: 24),
               ),
               subtitle: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     "Dosage: ${MedicationsScreen.dosages[index]}",
-                    style: TextStyle(
-                      color: Colors.white70,
-                      fontSize: 16,
-                    ),
+                    style: const TextStyle(color: Colors.white70, fontSize: 16),
                   ),
                   Text(
-                    "Time: ${int.parse(MedicationsScreen.times[index]) < 12 ? MedicationsScreen.times[index] : (int.parse(MedicationsScreen.times[index]) - 12).toString()} ${int.parse(MedicationsScreen.times[index]) < 12 ? 'AM' : 'PM'}",
-                    style: TextStyle(
-                      color: Colors.white70,
-                      fontSize: 16,
-                    ),
+                    "Time: ${int.parse(MedicationsScreen.times[index]) % 12 == 0 ? 12 : (int.parse(MedicationsScreen.times[index]) % 12).toString()} ${int.parse(MedicationsScreen.times[index]) < 12 ? 'AM' : 'PM'}",
+                    style: const TextStyle(color: Colors.white70, fontSize: 16),
                   ),
                   Row(
                     children: [
                       Text(
                         "Frequency: ",
-                        style: TextStyle(
+                        style: const TextStyle(
                           color: Colors.white70,
                           fontSize: 16,
                         ),
                       ),
                       Icon(
                         Icons.circle,
-                        color: MedicationsScreen.frequencies[index].split(', ')[0] == 'Yes'
-                        ? Colors.greenAccent : Colors.redAccent,
-                        size: 15
+                        color: MedicationsScreen.frequencies[index][0]
+                            ? Colors.greenAccent
+                            : Colors.redAccent,
+                        size: 15,
                       ),
                       Icon(
                         Icons.circle,
-                        color: MedicationsScreen.frequencies[index].split(', ')[1] == 'Yes'
-                        ? Colors.greenAccent : Colors.redAccent,
-                        size: 15
+                        color: MedicationsScreen.frequencies[index][1]
+                            ? Colors.greenAccent
+                            : Colors.redAccent,
+                        size: 15,
                       ),
                       Icon(
                         Icons.circle,
-                        color: MedicationsScreen.frequencies[index].split(', ')[2] == 'Yes'
-                        ? Colors.greenAccent : Colors.redAccent,
-                        size: 15
+                        color: MedicationsScreen.frequencies[index][2]
+                            ? Colors.greenAccent
+                            : Colors.redAccent,
+                        size: 15,
                       ),
                       Icon(
                         Icons.circle,
-                        color: MedicationsScreen.frequencies[index].split(', ')[3] == 'Yes'
-                        ? Colors.greenAccent : Colors.redAccent,
-                        size: 15
+                        color: MedicationsScreen.frequencies[index][3]
+                            ? Colors.greenAccent
+                            : Colors.redAccent,
+                        size: 15,
                       ),
                       Icon(
                         Icons.circle,
-                        color: MedicationsScreen.frequencies[index].split(', ')[4] == 'Yes'
-                        ? Colors.greenAccent : Colors.redAccent,
-                        size: 15
+                        color: MedicationsScreen.frequencies[index][4]
+                            ? Colors.greenAccent
+                            : Colors.redAccent,
+                        size: 15,
                       ),
                       Icon(
                         Icons.circle,
-                        color: MedicationsScreen.frequencies[index].split(', ')[5] == 'Yes'
-                        ? Colors.greenAccent : Colors.redAccent,
-                        size: 15
+                        color: MedicationsScreen.frequencies[index][5]
+                            ? Colors.greenAccent
+                            : Colors.redAccent,
+                        size: 15,
                       ),
                       Icon(
                         Icons.circle,
-                        color: MedicationsScreen.frequencies[index].split(', ')[6] == 'Yes'
-                        ? Colors.greenAccent : Colors.redAccent,
-                        size: 15
+                        color: MedicationsScreen.frequencies[index][6]
+                            ? Colors.greenAccent
+                            : Colors.redAccent,
+                        size: 15,
                       ),
                     ],
                   ),
@@ -118,11 +128,10 @@ class _MedicationsScreenState extends State<MedicationsScreen> {
               onTap: () {
                 // Action to view medication details
               },
-            )
+            ),
           );
-        }
+        },
       ),
-      
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
